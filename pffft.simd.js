7f0dc0cb7fccd453e239f0f1adf1cd045832e015
影桜 <79582228+vewz@users.noreply.github.com>
2024-04-14 21:23:02 +0900

    Add files via upload

--- pffft.simd.js
+++ pffft.simd.js
@@ -1,4 +1,3 @@
-
 var pffft_simd = (() => {
   var _scriptDir = typeof document !== 'undefined' && document.currentScript ? document.currentScript.src : undefined;
   if (typeof __filename !== 'undefined') _scriptDir = _scriptDir || __filename;

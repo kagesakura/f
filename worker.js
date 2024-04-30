0a85f9fe533ebc2fe03c5df8b50c0057c0877357
影桜 <79582228+kagesakura@users.noreply.github.com>
2024-04-30 16:47:26 +0900

    Update worker.js

--- worker.js
+++ worker.js
@@ -23,7 +23,7 @@ const promiseOfFFT = pffft_simd().then(Module => (audioBuffer, audioStepSize) =>
     dataHeap.set(new Uint8Array(audioBlock.buffer));
     Module._pffft_runner_transform_magnitudes(pffftRunner, dataHeap.byteOffset);
     const fftResult = new Float32Array(dataHeap.buffer, dataHeap.byteOffset, audioBlockSize / 2);
-    const magnitudes = fftResult.map(v => Math.log(v + 0.03));
+    const magnitudes = fftResult; // fftResult.map(v => Math.log(v + 0.03));
     stftMagnitudes.push(magnitudes);
   }
   Module._free(dataPtr);

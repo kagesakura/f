c055967d2e86002a4a640e3e770a575d1506e507
kagesakura <79582228+kagesakura@users.noreply.github.com>
2025-01-21 02:26:01 +0900

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,36 +1,33 @@
-importScripts("./pffft.simd.js");
+~async function() {
+  const { default: __wbg_init, new_fft, process_fft, drop_fft } = await import("./rustfft_wasm.js");
 
-const bytesPerElement = 4;
+  await __wbg_init({
+    module_or_path: await WebAssembly.compile(await (await fetch("./rustfft_wasm.wasm")).arrayBuffer())
+  });
 
-const promiseOfFFT = pffft_simd().then(Module => (audioBuffer, audioStepSize, audioBlockSize) => {
-  const audioSamples = new Float32Array(audioBuffer[0].length);
-  for (let i = 0; i < audioSamples.length; i++) {
-    let sum = 0;
-    for (let n = 0; n < audioBuffer.length; n++) {
-      sum += audioBuffer[n][i];
+  const doFft = (audioBuffer, audioStepSize, audioBlockSize) => {
+    const audioSamples = new Float32Array(audioBuffer[0].length);
+    for (let i = 0; i < audioSamples.length; i++) {
+      let sum = 0;
+      for (let n = 0; n < audioBuffer.length; n++) {
+        sum += audioBuffer[n][i];
+      }
+      audioSamples[i] = sum / audioBuffer.length;
     }
-    audioSamples[i] = sum / audioBuffer.length;
-  }
 
-  const stftMagnitudes = new Array();
-  const pffftRunner = Module._pffft_runner_new(audioBlockSize, bytesPerElement);
-  const nDataBytes = audioBlockSize * bytesPerElement;
-  const dataPtr = Module._malloc(nDataBytes);
-  const dataHeap = new Uint8Array(Module.HEAPU8.buffer, dataPtr, nDataBytes);
-  for(let audioSampleIndex = 0; audioSampleIndex < audioSamples.length - audioBlockSize; audioSampleIndex += audioStepSize){
-    const audioBlock = audioSamples.slice(audioSampleIndex, audioSampleIndex + audioBlockSize);
-    dataHeap.set(new Uint8Array(audioBlock.buffer));
-    Module._pffft_runner_transform_magnitudes(pffftRunner, dataHeap.byteOffset);
-    const fftResult = new Float32Array(dataHeap.buffer, dataHeap.byteOffset, audioBlockSize / 2);
-    const magnitudes = fftResult.map(v => Math.sqrt(Math.abs(v)));
-    stftMagnitudes.push(magnitudes);
-  }
-  Module._free(dataPtr);
-  Module._pffft_runner_destroy(pffftRunner);
-  return stftMagnitudes;
-});
+    const p = new_fft(audioBlockSize);
+    const stftMagnitudes = [];
+    for(let audioSampleIndex = 0; audioSampleIndex < audioSamples.length - audioBlockSize; audioSampleIndex += audioStepSize){
+      const audioBlock = audioSamples.slice(audioSampleIndex, audioSampleIndex + audioBlockSize);
+      const magnitudes = process_fft(p, audioBlock);
+      stftMagnitudes.push(magnitudes);
+    }
+    drop_fft(p);
+    return stftMagnitudes;
+  };
 
-self.addEventListener("message", async e => {
-  const { id, audioStepSize, data, audioBlockSize } = e.data;
-  self.postMessage({ id, data: (await promiseOfFFT)(data, audioStepSize, audioBlockSize) });
-});
+  self.addEventListener("message", async e => {
+    const { id, audioStepSize, data, audioBlockSize } = e.data;
+    self.postMessage({ id, data: doFft(data, audioStepSize, audioBlockSize) });
+  });
+}();
